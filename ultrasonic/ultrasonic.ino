9c3ab6a4bb4370c2828c69e558c44a2e5298d6f1
Samuel Marco <samuelmarco.2020@alunos.utfpr.edu.br>
2023-09-15 21:20:58 -0300

    feat

--- ultrasonic/ultrasonic.ino
+++ ultrasonic/ultrasonic.ino
@@ -1,18 +1,21 @@
 #include <Servo.h>
 Servo microServo;
 
-#define TRIGGER 4
-#define ECHO 3
-#define SERVO 2
+#define TRIGGER 8
+#define ECHO 9
+#define SERVO 11
+#define BUZZER 3
 
 int distance = 0;
 long duration = 0;
+int freq = 200;
   
 void setup() {
   // put your setup code here, to run once:
   pinMode(TRIGGER, OUTPUT);
   pinMode(ECHO, INPUT);
-  microServo.attach(SERVO, 544, 2400);
+  pinMode(BUZZER, OUTPUT);
+  microServo.attach(SERVO);
 
   Serial.begin(9600);
 }
@@ -26,10 +29,10 @@ void loop() {
     delay(30);
     calculateDistance();
     
-    Serial.print("Angulation: ");
-    Serial.println(index);
-    Serial.print("Distance: ");
-    Serial.println(distance);
+    Serial.print(i);
+    Serial.print(",");
+    Serial.print(distance);
+    Serial.print(".");
   }
   
   for(index = 170; index >= 10; index--) {
@@ -37,10 +40,10 @@ void loop() {
     delay(20);
     calculateDistance();
     
-    Serial.print("Angulation: ");
-    Serial.print(index);
-    Serial.print("Distance: ");
+    Serial.print(i);
+    Serial.print(",");
     Serial.print(distance);
+    Serial.print(".");
   }
 }
                
@@ -54,5 +57,15 @@ int calculateDistance() {
 
   duration = pulseIn(ECHO, HIGH);
   distance = duration*0.017175;
+  actBuzzer(distance);
+
   return distance;
+}
+
+void actBuzzer(int dist) {
+  if(dist <= 20) {
+    tone(BUZZER, freq);
+  } else{
+    noTone(BUZZER);
+  }
 }
